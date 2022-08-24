7448469cb56cd9630d8e36c6c6508b5a854bc544
mshtelma <mshtelma>
2022-08-24 07:23:06 +0000

    data marts

--- etl.sql
+++ etl.sql
@@ -119,12 +119,14 @@ CREATE INCREMENTAL LIVE TABLE catalog_sales_curated (
 PARTITIONED BY (cs_sold_date_sk)
 
 -- COMMAND ----------
+
 CREATE STREAMING LIVE VIEW catalog_sales_landing (
 
 )
 COMMENT "Catalog Sales Landing"
 AS SELECT *
 FROM STREAM(tpcds1gb.catalog_sales);
+
 -- COMMAND ----------
 
 CREATE STREAMING LIVE TABLE catalog_sales_staging (
@@ -181,7 +183,7 @@ FROM
 
 -- COMMAND ----------
 
-CREATE INCREMENTAL LIVE TABLE web_sales (
+CREATE INCREMENTAL LIVE TABLE web_sales_curated (
   ws_sold_date_sk INT,
   ws_sold_time_sk INT,
   ws_ship_date_sk INT,
@@ -221,12 +223,14 @@ PARTITIONED BY (ws_sold_date_sk)
 
 
 -- COMMAND ----------
+
 CREATE STREAMING LIVE VIEW web_sales_landing (
 
 )
 COMMENT "Web Sales Landing"
 AS SELECT *
 FROM STREAM(tpcds1gb.web_sales);
+
 -- COMMAND ----------
 
 CREATE STREAMING LIVE TABLE web_sales_staging (
@@ -275,3 +279,84 @@ SELECT
 FROM
   STREAM(live.web_sales_staging) ws
   LEFT ANTI JOIN live.date_dim_curated dd2 on ws.ws_ship_date_sk = dd2.d_date_sk
+
+-- COMMAND ----------
+
+-- MAGIC %md # Data Marts
+
+-- COMMAND ----------
+
+CREATE LIVE TABLE data_mart_60 AS with cs as (
+  select
+    i_item_id,
+    sum(cs_ext_sales_price) total_sales
+  from
+    live.catalog_sales_curated,
+    live.date_dim_curated,
+    live.customer_address_curated,
+    live.item_curated
+  where
+    i_item_id in (
+      select
+        i_item_id
+      from
+        live.item_curated
+      where
+        i_category in ('Music')
+    )
+    and cs_item_sk = i_item_sk
+    and cs_sold_date_sk = d_date_sk
+    and d_year = 1998
+    and d_moy = 9
+    and cs_bill_addr_sk = ca_address_sk
+    and ca_gmt_offset = -5
+  group by
+    i_item_id
+),
+ws as (
+  select
+    i_item_id,
+    sum(ws_ext_sales_price) total_sales
+  from
+    live.web_sales_curated,
+    live.date_dim_curated,
+    live.customer_address_curated,
+    live.item_curated
+  where
+    i_item_id in (
+      select
+        i_item_id
+      from
+        live.item_curated
+      where
+        i_category in ('Music')
+    )
+    and ws_item_sk = i_item_sk
+    and ws_sold_date_sk = d_date_sk
+    and d_year = 1998
+    and d_moy = 9
+    and ws_bill_addr_sk = ca_address_sk
+    and ca_gmt_offset = -5
+  group by
+    i_item_id
+)
+select
+  i_item_id,
+  sum(total_sales) total_sales
+from
+  (
+    select
+      *
+    from
+      cs
+    union all
+    select
+      *
+    from
+      ws
+  ) tmp1
+group by
+  i_item_id
+order by
+  i_item_id,
+  total_sales -- limit 100

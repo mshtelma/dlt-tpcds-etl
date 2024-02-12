9bfd4c579c6b8374cc5312ddb3c141d7bd530d03
Michael Shtelma <michael.shtelma@databricks.com>
2024-02-12 12:17:33 +0100

    New repo structure + introducing python tools

---
+++ setup.py
@@ -0,0 +1,15 @@
+import os
+
+from setuptools import setup, find_packages
+
+setup(
+    name="dlt_tpc_ds_demo",
+    version="0.0.1",
+    description="ETL for TPC-DS dataset using Databricks Delta Live Tables",
+    author="Michael Shtelma",
+    url="",
+    install_requires=[],
+    packages=find_packages(),
+    long_description="",
+    long_description_content_type="text/markdown",
+)
